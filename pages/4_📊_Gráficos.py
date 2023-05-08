679f2c43cc94cb13accc180419580582b83335e1
Esteban Navarro-Díaz <74066277+Esteban0186@users.noreply.github.com>
2023-05-07 23:40:33 -0600

    Update 4_📊_Gráficos.py

--- pages/4_📊_Gráficos.py
+++ pages/4_📊_Gráficos.py
@@ -26,11 +26,11 @@ with tab1:
         else:
                
                conteos = pd.DataFrame(df[var_cual].value_counts())
-               fig1 = px.bar(conteos, x=conteos.index, y= var_cual, 
-                            orientation= "h",
-                            title= "Cantidad de jugadores por la variable {}".format(var_cual),
-                            labels={"Cantidad": "Cantidad de jugadores", var_cual: str(var_cual)},
-                            text= "Cantidad",height=400)
+               fig1 = px.bar(conteos, x=conteos.index, y= var_cual)#, 
+                            #orientation= "h",
+                            #title= "Cantidad de jugadores por la variable {}".format(var_cual),
+                            #labels={"Cantidad": "Cantidad de jugadores", var_cual: str(var_cual)},
+                            #text= "Cantidad",height=400)
                 
 
             
